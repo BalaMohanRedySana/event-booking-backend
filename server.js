14623a7359f2418e3138e57cf993ea9207049b24
BalaMohanRedySana <balamohan5886@gmail.com>
2026-01-08 15:45:54 +0530

    Fix CORS configuration for production deployment

--- server.js
+++ server.js
@@ -37,8 +37,11 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   })
 );
 
